b0c817b5d78608a2e2855184a8b445d4179544db
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2018-05-23 09:57:54 +0200

    finalize v2

--- static/js/bgen.js
+++ static/js/bgen.js
@@ -2,35 +2,36 @@ const submitButton = document.querySelector('#btn-submit')
 const exampleButton = document.querySelector('#btn-example')
 const inputLength = document.querySelector('#barcodeLength')
 const inputAmount = document.querySelector('#barcodeAmount')
+const resultsContainer = document.querySelector('#results-container')
 const resultSequences = document.querySelector('#barcode-sequences')
-const frequencyChart = document.querySelector('#frequency-chart')
 const sequenceStats = document.querySelector('#sequence-stats')
 const notification = document.querySelector('#bgen-notification')
  
 submitButton.addEventListener('click', run)
 exampleButton.addEventListener('click', showExample)
 
+const worker = new Worker('static/js/generateBarcodes.js')
+worker.onmessage = function (event) {
+  const result = event.data
+  hideElement(notification)
+  displayResults(result)
+}
+
 function run () {
   const barcodeLength = Number.parseInt(inputLength.value, 10)
   const barcodeAmount = Number.parseInt(inputAmount.value, 10)
   
   showElement(notification)
-  hideElement(resultSequences)
-  hideElement(frequencyChart)
+  hideElement(resultsContainer)
 
-  const result = generate(barcodeLength, barcodeAmount)
-  setTimeout(() => {
-    hideElement(notification)
-    displayResults(result)
-  }, 3500)
-}
-
-// FIXME port Python code
-function generate (length, amount) {
-  return exampleData
+  worker.postMessage({
+    length: barcodeLength,
+    amount: barcodeAmount
+  })
 }
 
 function displayResults (result) {
+  showElement(resultsContainer)
   baseFrequencies = computeBaseFrequencies(result.barcodes)
   displaySequences(result.barcodes)
   displayStats(result)
@@ -39,13 +40,14 @@ function displayResults (result) {
 
 function displaySequences (sequences) {
   resultSequences.textContent = sequences.join('\n')
-  showElement(resultSequences)
 }
 
 function displayStats (result) {
-  let t = '<table class="table"><tbody>'
+  let t = '<table class="table table-sm table-hover"><tbody>'
+  t += '<tr><td>Length of barcodes</td><td>' + result.barcodeLength + '</td></tr>'
+  t += '<tr><td>Amount of barcodes</td><td>' + result.barcodeCount + '</td></tr>'
   t += '<tr><td>Min. Hamming distance</td><td>' + result.pairwiseHammingDistance + '</td></tr>'
-  t += '<tr><td>Squared error</td><td>' + result.squaredError + '</td></tr>'
+  t += '<tr><td>Squared error</td><td>' + result.squaredError.toFixed(4) + '</td></tr>'
   t += '</tbody></table>'
   sequenceStats.innerHTML = t
 }
@@ -85,6 +87,9 @@ function plotFrequencies (frequencies) {
     const base = bases[i]
     data.push({
       y: frequencies[base],
+      x: frequencies[base].map(function (_, i) {
+        return i + 1
+      }),
       name: base,
       type: 'bar'
     })
@@ -92,43 +97,42 @@ function plotFrequencies (frequencies) {
 
   const layout = {
     barmode: 'stack',
+    title: 'Per-position base distribution',
     xaxis: {
-      title: 'Position'
+      title: 'Position',
+      tickvals: data[0].x
     },
     yaxis: {
       title: 'Frequency'
     }
   }
 
-  showElement(frequencyChart)
   Plotly.newPlot('frequency-chart', data, layout)
 }
 
 function showElement (element) {
   element.classList.remove('d-none')
-  // element.classList.add('visible')
 }
 
 function hideElement (element) {
   element.classList.add('d-none')
-  // element.classList.remove('visible')
 }
 
 const exampleData = {
-  "barcodeCount": 8, 
-  "barcodeLength": 10, 
-  "barcodes": [
-    "GTAGCGTAAC", 
-    "GATCGCACTT", 
-    "CTCTAACGGA", 
-    "GGCACAATTC", 
-    "CCTGATGTAG", 
-    "AAGGCTTCCA", 
-    "AGCCTCTAGT", 
-    "TACTAGATCG"
+  barcodeCount: 8,
+  barcodeLength: 10,
+  barcodes: [
+    'GTAGCGTAAC',
+    'GATCGCACTT',
+    'CTCTAACGGA',
+    'GGCACAATTC',
+    'CCTGATGTAG',
+    'AAGGCTTCCA',
+    'AGCCTCTAGT',
+    'TACTAGATCG'
   ], 
-  "pairwiseHammingDistance": 7, 
-  "squaredError": 0.034375
+  pairwiseHammingDistance: 7,
+  squaredError: 0.034375
 }
 
 function showExample () {
---
+++ static/js/generateBarcodes.js
@@ -0,0 +1,132 @@
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.10/lodash.min.js')
+
+onmessage = function (event) {
+  const result = generate(event.data.length, event.data.amount)
+  postMessage(result)
+}
+
+function generate (length, amount) {
+  const alphabet = 'ACGT'
+  let candidates = alphabet.split('')
+
+  for (let i = 1; i < length; i += 1) {
+    const newCandidates = []
+    for (let j = 0; j < candidates.length; j += 1) {
+      const seq = candidates[j]
+      if (seq.length < 8 || (isBalanced(seq) && Math.random() > length / 22)) {
+        for (let k = 0; k < alphabet.length; k += 1) {
+          newCandidates.push(seq + alphabet[k])
+        }
+      }
+    }
+    candidates = newCandidates
+  }
+
+  if (candidates.length > 50000) {
+    candidates.sort(function (s1, s2) {
+      return entropy(s2) - entropy(s1)
+    })
+    candidates = candidates.splice(0, 50000)
+  }
+
+  if (amount >= candidates.length) {
+    amount = candidates.length - 1
+  }
+
+  let barcodes = []
+  let bestHamming = 0
+  let bestBaseDist = 10
+  let iterCount = 0
+  let freqCheck = Math.max(1, Math.floor(10000 / amount))
+  const startTimeS = (new Date()).getTime() / 1000
+
+  while (true) {
+    if (iterCount % freqCheck === 0) {
+      const nowS = (new Date()).getTime() / 1000
+      if (nowS > startTimeS + 20) {
+        break
+      }
+    }
+
+    const subset = _.sampleSize(candidates, amount)
+    const distances = []
+    for (let i = 0; i < subset.length - 1; i += 1) {
+      for (let j = i + 1; j < subset.length; j += 1) {
+        distances.push(hammingDistance(subset[i], subset[j]))
+      }
+    }
+    const minHamming = _.min(distances)
+    const baseDistSubset = baseDist(subset)
+
+    if (minHamming > bestHamming || (minHamming === bestHamming && bestBaseDist > baseDistSubset)) {
+      bestHamming = minHamming
+      bestBaseDist = baseDistSubset
+      barcodes = subset
+      iterCount = 0
+    } else {
+      iterCount += 1
+    }
+  }
+  
+  return {
+    barcodeCount: amount,
+    barcodeLength: length,
+    barcodes: barcodes,
+    pairwiseHammingDistance: bestHamming, 
+    squaredError: bestBaseDist
+  }
+}
+
+function isBalanced (seq) {
+  const countThreshold = seq.length / 2
+  const counts = { A: 0, C: 0, G: 0, T: 0 }
+  for (i = 0; i < seq.length; i += 1) {
+    counts[seq[i]] += 1
+    if (counts[seq[i]] >= countThreshold) {
+      return false
+    }
+  }
+  return true
+}
+
+function entropy (seq) {
+  const counter = {}
+  for (const char of seq) {
+    if (counter[char] === undefined) {
+      counter[char] = 0
+    }
+    counter[char] += 1
+  }
+  let ret = 0
+  for (const char in counter) {
+    const freq = counter[char] / seq.length
+    ret += freq * (Math.log(freq) / Math.log(2))
+  }
+  return ret === 0 ? 0 : -ret
+}
+
+function hammingDistance (s1, s2) {
+  let d = 0
+  for (let i = 0; i < s1.length; i += 1) {
+    if (s1[i] !== s2[i]) {
+      d += 1
+    }
+  }
+  return d
+}
+
+function baseDist (seqs) {
+  let error = 0
+  const numSeqs = seqs.length
+  const seqLength = seqs[0].length
+  for (let i = 0; i < seqLength; i += 1) {
+    const freqs = { A: 0, C: 0, G: 0, T: 0 }
+    for (let j = 0; j < numSeqs; j += 1) {
+      const seq = seqs[j]
+      freqs[seq[i]] += 1
+    }
+    error += Math.pow(0.25 - freqs.A / numSeqs, 2) + Math.pow(0.25 - freqs.C / numSeqs, 2)
+    error += Math.pow(0.25 - freqs.G / numSeqs, 2) + Math.pow(0.25 - freqs.T / numSeqs, 2)
+  }
+  return error / seqLength
+}
